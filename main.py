7b954a96b8339d28587787a5ce428306431280bd
BernardMoy <bernardmoy3@gmail.com>
2025-06-03 18:31:06 +0100

    Pokemon regex matching

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+import requests
+import re
 
 load_dotenv()
 
@@ -29,9 +31,22 @@ async def on_message(message):
     # Listen to hint messages from poketwo
     if message.content.startswith("The pokémon is "):
         # Extract the hint from the message
-        hint = message.content[14:-1]
-        hint.replace("\\", "")  # Remove all backslashes
+        hint = message.content[15:-1].lower()
+        hint = hint.replace("\_", ".")  # Replace _ with . for regex matching
         await message.channel.send(hint)
+
+        # Iterate over the currently 1025 pokemons using the poke API
+        # Filter them by regex matching
+        response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=10000")
+        data = response.json()
+
+        reply = ""
+        for pokemon in data["results"]:
+            if re.fullmatch(hint, pokemon["name"].lower()):
+                reply += pokemon["name"] + "\n"
+
+        # Create embed 
+        await message.channel.send(reply)
 
     # Allow listening to message continuously
     await bot.process_commands(message)
@@ -39,6 +54,14 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"{ctx.author.mention} Pong! {bot.latency * 1000:.2f}ms")
+
+@bot.command()
+async def dm(ctx, *, msg):  # msg refers to the message (Parameters) after the command
+    await ctx.author.send(msg)   # DM private message the author
+
+@bot.command()
+async def reply(ctx, *, msg):
+    await ctx.reply(msg)  # Reply with the user message
 
 # Run the bot at the end
 bot.run(token, log_level = logging.DEBUG)
