ffb9608cd6a47f0f1e4214a62cbe3bc649664977
BernardMoy <bernardmoy3@gmail.com>
2025-06-29 11:34:09 +0800

    Board class and print method

--- cogs/games/sudoku.py
+++ cogs/games/sudoku.py
@@ -2,12 +2,61 @@ from discord.ext import commands
 import discord
 import re
 
+emoji_map = {
+        0: ":black_large_square:",
+        1: ":one:",
+        2: ":two:",
+        3: ":three:",
+        4: ":four:",
+        5: ":five:",
+        6: ":six:",
+        7: ":seven:",
+        8: ":eight:",
+        9: ":nine:"
+    }
+
+# A class to store sudoku board
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def get_row(self, x, y):
+        return self.board[x]
+
+    def get_col(self, x, y):
+        return [self.board[x][y] for x in range(len(self.board))]
+
+    def get_square(self, x, y):
+        elements = []
+        for h in range(3*(x//3), 3*(x//3)+3):
+            for w in range(3*(y//3), 3*(y//3)+3):
+                elements.append(self.board[h][w])
+        return elements
+
+    # Given a position x,y determine the possible set of numbers there
+    def get_available(self, x, y):
+        avail = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        banned = self.get_row(x, y).union(self.get_col(x, y)).union(self.get_square(x, y))
+        return avail.difference(banned)
+
+    # Method to print the board with a single hint position
+    def to_string(self, hint_x, hint_y):
+        s = ''
+        for row in range(len(self.board)):
+            for col in range(len(self.board[row])):
+                if row == hint_x and col == hint_y:
+                    s += ':large_orange_diamond:'
+                else:
+                    s += emoji_map[self.board[row][col]]
+            s += '\n'
+        return s
+
 class Sudoku(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.hybrid_command(name="hintsudoku", description="Give a hint to a sudoku puzzle for the Discord game. Use 0 for unknowns ", with_app_command=True)
-    async def hintsudoku(self, ctx, *, board_str):
+    async def hintsudoku(self, ctx, *, board_str = ''):
         # Trim the board
         board_str = board_str.strip()
 
@@ -36,7 +85,10 @@ class Sudoku(commands.Cog):
             await ctx.send(embed=embed_error)
             return
 
-        await ctx.send(board)
+        # Create the board object
+        sudoku_board = Board(board)
+        await ctx.send(sudoku_board.to_string(2,3))
+
 
 async def setup(bot):
     await bot.add_cog(Sudoku(bot))
