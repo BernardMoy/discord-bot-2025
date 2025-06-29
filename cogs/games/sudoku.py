7cc017af1875e0902d007335d6d02759bbb8c68a
BernardMoy <bernardmoy3@gmail.com>
2025-06-29 11:09:07 +0800

    Send error if incorrect sudoku format

--- cogs/games/sudoku.py
+++ cogs/games/sudoku.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import re
 
 class Sudoku(commands.Cog):
@@ -13,10 +14,27 @@ class Sudoku(commands.Cog):
         # Create the board from the string provided
         board = []
 
+        # Load into board and check for correct format
+        embed_error = discord.Embed(
+                title="Usage",
+                description=f"`-hintsudoku 000005702 090700030...`",
+                color=discord.Color(int("f5429e", 16))
+            )
+        embed_error.set_footer(text = "Use 0 for the unknowns.")
+
         for row in re.split(r' |\n', board_str):
             cur_row = [int(s) for s in list(row)]
+
+            if cur_row and len(cur_row) != 9:
+                await ctx.send(embed=embed_error)
+                return
+
             if cur_row:
                 board.append(cur_row)
+
+        if len(board) != 9:
+            await ctx.send(embed=embed_error)
+            return
 
         await ctx.send(board)
 
